7dffad77f62d81553a819ea80c8eac61ebdcda93
MAHAK GUPTA <55080310+mahak-dev@users.noreply.github.com>
2022-01-27 21:45:41 +0530

    Add files via upload

--- TestThread.java
+++ TestThread.java
@@ -1,41 +1,44 @@
 import java.util.*;
- class RunnableDemo implements Runnable {
-     private Thread t;
-     private String threaddName;
 
-     RunnableDemo (String name) {
-         threaddName = name;
-         System.out.println("creating" + threaddName);
-     }
-    public void run() {
-        System.out.println("Running" + threaddName );
+class ThreadDemo extends Thread {
+    private Thread t;
+    private String threadName;
+
+    ThreadDemo(String name){
+        threadName = name;
+        System.out.println("Creating " + threadName );
+    }
+    public void run(){
+        System.out.println("Running " + threadName);
         try {
-            for(int i = 4 ; i > 0 ; i--){
-                System.out.println("Thread: "+ threaddName + " , " + i);
+            for(int i = 4; i > 0 ; i--) {
+                System.out.println("Thread:" + threadName + " ," + i);
                 Thread.sleep(50);
             }
         } catch (InterruptedException e) {
-            System.out.println("Thread " + threaddName + "interrupted.");
+            System.out.println("Thread " + threadName + "exiting.");
+            //TODO: handle exception
         }
-        System.out.println("Thraed" + threaddName + "exiting.");
+         System.out.println("Thread " + threadName + "exiting.");
     }
-    public void start ()
-    {
-     System.out.println("Starting" + threaddName);
-     if(t == null)
-     {
-         t = new Thread(this,threaddName);
-         t.start();
-     }
+        public void start ()
+        {
+            System.out.println("Starting" + threadName);
+            if (t == null)
+            {
+                t = new Thread(this. threadName);
+                t.start ();
+            }
+        }
     }
- }
+    public class TestThread {
+        public static void main(String args[]){
+            
+            ThreadDemo T1 = new ThreadDemo("Thread-1");
+            T1.start();
 
- public class TestThread {
-     public static void main(String args[]) {
-         RunnableDemo R1 = new RunnableDemo("Thread-1");
-         R1.start();
+            ThreadDemo T2 = new ThreadDemo("Thread-2");
+            T2.start();
+        }
+    }
 
-         RunnableDemo R2 = new RunnableDemo("Thread-2");
-         R2.start();
-     }
- }
