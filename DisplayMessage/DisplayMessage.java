fc382e1206797acec842f80af374695eb1c494e9
MAHAK GUPTA <55080310+mahak-dev@users.noreply.github.com>
2022-01-27 22:35:03 +0530

    Add files via upload

---
+++ DisplayMessage/DisplayMessage.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class DisplayMessage implements Runnable
+{
+    private String message;
+    public DisplayMessage(String message)
+    {
+        this.message = message;
+    }
+    public void run()
+    {
+        while(true)
+        {
+            System.out.println(message);
+        }
+    }
+}
