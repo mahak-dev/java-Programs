75bb5a8a1e3128acba0c9004f07137505d1c93a3
MAHAK GUPTA <55080310+mahak-dev@users.noreply.github.com>
2022-01-28 01:25:51 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Customer{
+    int amount = 10000;
+
+synchronized void withdraw(int amount){
+    System.out.println("Going to withdraw...");
+
+if(this.amount<amount){
+    System.out.println("Less balance; waiting for Deposit...");
+    try {
+        wait();
+    } catch (Exception e) {
+    }
+}
+this.amount-=amount;
+System.out.println("withdraw completed...");
+}
+
+synchronized void deposit(int amount){
+    System.out.println("going to deposit...");
+    this.amount+=amount;
+    System.out.println("deposit completed...");
+    notify();
+}
+}
+
+class Test{
+    public static void main(String args[]){
+        final Customer c = new Customer();
+        new Thread(){
+            public void run(){
+                c.withdraw(15000);
+            }
+        }.start();
+        new Thread(){
+            public void run(){
+                c.deposit(10000);
+            }
+        }.start();
+    }
+}
