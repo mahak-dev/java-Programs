30493f16061d435cdd3832e6609f13b51787b474
MAHAK GUPTA <55080310+mahak-dev@users.noreply.github.com>
2022-01-28 21:21:21 +0530

    Add files via upload

---
+++ TestInterruptingThread1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class TestInterruptingThread1 extends Thread{
+    public void run(){
+        try{
+            Thread.sleep(1000);
+            System.out.println("task");
+        }
+        catch(InterruptedException e){
+            throw new RuntimeException("Thread interrupted..."+e);
+        }
+    }
+
+    public static void main(String args[]){
+        TestInterruptingThread1 t1=new TestInterruptingThread1();
+        t1.start();
+        try {
+            t1.interrupted();
+        } catch (Exception e) {
+            System.out.println("Exception handled" + e);
+        } 
+    }
+}
