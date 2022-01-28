813ee9cec6149cef22d4d4ef3ec47c64b2c032cc
MAHAK GUPTA <55080310+mahak-dev@users.noreply.github.com>
2022-01-29 01:48:53 +0530

    Add files via upload

---
+++ TestDaemonThread1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class TestDaemonThread1 extends Thread{
+    public void run(){
+        if(Thread.currentThread().isDaemon()){
+            System.out.println("daemon thread work");
+        }
+        else{
+            System.out.println("user thread work");
+        }
+    }
+    public static void main(String args[]){
+        TestDaemonThread1 t1=new TestDaemonThread1();
+        TestDaemonThread1 t2=new TestDaemonThread1();
+        TestDaemonThread1 t3=new TestDaemonThread1();
+
+        t1.setDaemon(true);//now t1 is daemon thread
+
+        t1.start();//starting threads
+        t2.start();
+        t3.start();
+    }
+}
