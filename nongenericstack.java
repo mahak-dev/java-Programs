c197720afa1072f2db9d23e6d6dabe339a95ea5a
MAHAK GUPTA <55080310+mahak-dev@users.noreply.github.com>
2022-02-03 01:38:57 +0530

    Add files via upload

---
+++ nongenericstack.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class nongenericstack
+{
+    public static void main (String args[]){
+        Stack s_num = new Stack();
+
+        /*s_num.push(1);
+        s_num.push(2);
+        s_num.push(3);
+        s_num.push(4);*/
+
+Integer s1 = (Integer) s_num.push(0);
+Integer s2 = (Integer) s_num.push(1);
+Integer s3 = (Integer) s_num.push(2);
+Integer s4 = (Integer) s_num.push(3);
+System.out.println(s1);
+System.out.println(s2);
+System.out.println(s3);
+System.out.println(s4);
+    }
+}
